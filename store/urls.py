1acc9072f5a3d54ac3497a11613ab57e0fc5d3aa
abhi <abhicse002@gmail.com>
2021-05-06 20:12:33 +0530

    changed product url slug field

--- store/urls.py
+++ store/urls.py
@@ -6,6 +6,6 @@ app_name = "store"
 urlpatterns = [
     path('', views.products_all , name='products_all'),
     #<slug:slug> --- datatype:nameof the refer point
-    path('book/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('<slug:slug>/', views.product_detail, name='product_detail'),
     path('category/<slug:category_slug>/', views.category_detail, name='category_detail')
 ]
